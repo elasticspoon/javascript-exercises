b82a5fe3838ae0d5c18eb9024ed5c20f378dc60b
Ben Dover <quesadillaman@gmail.com>
2020-09-24 03:27:19 -0400

    First 3 exercises done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberRepeats) {
+    let resultingString = '';
+    if (numberRepeats < 0) return 'ERROR';
+    for (let i = 0; i < numberRepeats; i++)
+    {
+        resultingString += stringToRepeat;
+    }
+    return resultingString;
 }
 
 module.exports = repeatString
