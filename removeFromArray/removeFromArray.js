af43f19f9ef1c9615e46a1076510c1bab3e6ab53
Ben Dover <quesadillaman@gmail.com>
2020-09-24 21:44:52 -0400

    2 more exercises done

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(targetArray, ...removalTargets) {
+    let arraySansTargets = targetArray;
+    for (let i = 0; i < removalTargets.length; i++) {
+        const equalArrayElement = element => element === removalTargets[i];
+        let indexOfRemovalTarget = arraySansTargets.findIndex(equalArrayElement);
+        if (indexOfRemovalTarget != -1) {
+            arraySansTargets.splice(indexOfRemovalTarget, 1);
+        }
+    }
+    return arraySansTargets;
 }
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(integerA, integerB) {
+    if (integerA < 0 || integerB < 0) return "ERROR";
+    if (typeof integerA != Number || typeof integerB != Number) return "ERROR";
+    const lowerInt = integerA < integerB ? integerA : integerB;
+    const higherInt = integerA > integerB ? integerA : integerB;
+    
+    let cumulativeSum = 0;
 
+    for (let i = lowerInt; i <= higherInt; i++) {
+        cumulativeSum += i;
+    }
+    return cumulativeSum;
 }
 
 module.exports = sumAll
